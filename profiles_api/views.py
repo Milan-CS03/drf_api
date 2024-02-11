50cd6f7f53d2ebea4894a1caa0c02c695e5635a2
milan <milan.mech.iitb@gmail.com>
2024-02-11 17:17:23 +0530

    commit for get api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
+
+class HelloApiView(APIView):
+    """test api views"""
+
+    def get(self, request, format=None):
+        """return list of api features"""
+        an_api_view = ['User http methods as function( get, post, patch)',
+                       'is similar to Django views',
+                       'gives more control over application logic',
+                       ]
+        return Response({'message': 'hello', 'an_api_view': an_api_view})
+
